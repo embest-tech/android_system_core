1475b14006be1af522d90319314374b254ab2e53
Your Name <you@example.com>
2017-10-17 18:06:05 +0800

    Add: can command tools

---
+++ canconfig/Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= canconfig.c libsocketcan.c
+
+LOCAL_MODULE:= canconfig
+
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+
+LOCAL_STATIC_LIBRARIES := libc
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+LOCAL_MODULE_TAGS := debug
+
+include $(BUILD_EXECUTABLE)
